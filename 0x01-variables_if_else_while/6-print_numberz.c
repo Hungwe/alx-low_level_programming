e9900fefb00e522d1bd8bcc95507e309050b8834
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-17 16:36:54 -0700

    Print numbers not using char variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+
+return (0);
+}
