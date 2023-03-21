7b2b249b479ba8aa2452d77007a501367427ca44
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-21 15:47:13 -0700

    Print sign of given value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 printf("+\n");
+return (1);
 }
 if (n == 0)
 {
-return (0);
 printf("0\n");
+return (0);
 }
 else
 {
-return (-1);
 printf("-\n");
+return (-1);
 }
 }
