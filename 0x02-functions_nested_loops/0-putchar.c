d97490364bb251e59275dbac1a4e6f7e6512975b
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-21 03:12:01 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
