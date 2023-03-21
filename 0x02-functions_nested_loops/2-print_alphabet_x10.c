277c96adbdc2db86ec885191a93ce152fbf7c2c8
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-21 13:37:37 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet in small caps x10
+ *
+ * Return: alphabet x10.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("abcdefghijklmnopqrstuvwxyz\n");
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 char print_alphabet(void);
+void print_alphabet_x10(void);
