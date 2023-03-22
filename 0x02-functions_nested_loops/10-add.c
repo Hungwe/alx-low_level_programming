a09fe2e0a6c320c3c8b541dddafbcd5b986bbf29
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-21 18:17:26 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * add - Print last digit of number
+ * @a: Input parameter
+ * @b: Second input parameter
+ *
+ * Return: sum of two numbers
+ */
+
+int add(int a, int b)
+{
+int y = a + b;
+return (y);
+}
