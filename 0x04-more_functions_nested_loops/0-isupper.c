9500c1704aa8558032a071ef4830de49987528e3
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-24 04:33:27 -0700

    Check if char is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *_isupper - Checks if letter is upper case
+ * @c: input character
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+int a;
+a = isupper(c);
+return (a);
+}
