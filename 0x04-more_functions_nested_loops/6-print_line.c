d0cc3cb0475cefc7de0ddb351a989353fab3ef53
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-24 08:51:33 -0700

    Print lines relative to a number n

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_line - print lines relative to number n
+ * @n: input number
+ *
+ */
+
+void print_line(int n)
+{
+if (n <= 0)
+{
+printf("$\n");
+}
+else
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("_");
+}
+printf("$\n");
+}
+}
