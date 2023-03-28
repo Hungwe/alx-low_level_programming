1630473a3090c8097eb56956124f0a81a380206a
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-27 19:51:46 -0700

    Print a part of an array using pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * puts_half - prints out a part of a string using pointers
+ * @str: Input string pointer
+ *
+ */
+
+void puts_half(char *str)
+{
+int l;
+int i;
+l = strlen(str);
+if (l % 2 == 0)
+{
+int half;
+half = l / 2;
+for (i = half; i < l; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
+else if (l % 2 != 0)
+{
+int n;
+int sub;
+sub = l - 1;
+n = sub / 2;
+for (i = n; i < l; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
+}
