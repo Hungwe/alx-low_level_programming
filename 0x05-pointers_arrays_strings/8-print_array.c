323af062521b708b8ea1b3714a08441a4832cbb4
SE_Taonanyasha <taonashuro@gmail.com>
2023-03-28 07:30:59 -0700

    Print a number of elements from an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,10 +16,17 @@ void print_array(int *a, int n)
 {
 int i;
 int temp;
+if (n > 0)
+{
 for (i = 0; i < n - 1; i++)
 {
 printf("%d, ", a[i]);
 }
 temp = n - 1;
 printf("%d\n", a[temp]);
+}
+else
+{
+printf("\n");
+}
 }
