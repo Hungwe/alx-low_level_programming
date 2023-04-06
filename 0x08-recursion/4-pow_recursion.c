08fdf646c3b85005a539a1254bccfadbfa710ae5
SE_Taonanyasha <taonashuro@gmail.com>
2023-04-06 06:48:55 -0700

    Find x to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Return x raised to the power of y
+ * @x: Base
+ * @y: Exponent
+ *
+ * Return: Number
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
