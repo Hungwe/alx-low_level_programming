6f5567ee2dd64d2232352bf8979cf5e3f4e0d049
SE_Taonanyasha <taonashuro@gmail.com>
2023-04-17 05:49:16 -0700

    Dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A database of dogs
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: Database for dogs
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif // DOG_H
