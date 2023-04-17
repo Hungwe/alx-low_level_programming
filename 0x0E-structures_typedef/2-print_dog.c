e792f9f836b3c9e4600ccdf501bd1ac8e55d88c8
SE_Taonanyasha <taonashuro@gmail.com>
2023-04-17 12:27:14 -0700

    Print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,9 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include "dog.h"
-#include "main.h"
 
 /**
  * print_dog - Print dog profile
@@ -13,7 +13,44 @@
 
 void print_dog(struct dog *d)
 {
+if ((*d).name == NULL && (*d).age != 0 && (*d).name != NULL)
+{
+printf("Name: (nil)\n");
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", (*d).owner);
+}
+else if ((*d).age == 0 && (*d).name != NULL && (*d).owner != NULL)
+{
+printf("Name: %s\n", (*d).name);
+printf("Age: (nil)\n");
+printf("Owner: %s\n", (*d).owner);
+}
+else if ((*d).owner == NULL && (*d).age != 0 && (*d).name != NULL)
+{
+printf("Name: %s\n", (*d).name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: (nil)\n");
+}
+else if ((*d).owner == NULL && (*d).age != 0 && (*d).name == NULL)
+{
+printf("Name: (nil)\n");
+printf("Age: %f\n", (*d).age);
+printf("Owner: (nil)\n");
+}
+else if ((*d).age == 0 && (*d).name == NULL && (*d).owner != NULL)
+{
+printf("Name: (nil)\n");
+printf("Age: (nil)\n");
+printf("Owner: %s\n", (*d).owner);
+}
+else if ((*d).name == NULL && (*d).age == 0 && (*d).owner == NULL)
+{
+return;
+}
+else
+{
 printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
 printf("Owner: %s\n", (*d).owner);
+}
 }
