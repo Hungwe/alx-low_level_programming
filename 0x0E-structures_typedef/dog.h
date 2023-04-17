e9b0e68e6798253bb4defeb7a633a111c97acba4
SE_Taonanyasha <taonashuro@gmail.com>
2023-04-17 12:45:37 -0700

    New type for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,9 @@ char *owner;
 };
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 #endif
