1e8c8f679b830e4e62bebc367cfef48de93a2b2b
SE_Taonanyasha <taonashuro@gmail.com>
2023-04-20 02:21:56 -0700

    Act on array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,4 +5,6 @@ void _putchar(char x);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+void print_elem(int elem);
+void print_elem_hex(int elem);
 #endif
