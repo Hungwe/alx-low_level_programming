4d494b2d6ca6152d28edd8b8adc2e916c202a5e6
Hungwe <taonashuro@gmail.com>
2023-10-11 10:41:02 -0700

    Preprocessing Tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - Print name of source code file
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
