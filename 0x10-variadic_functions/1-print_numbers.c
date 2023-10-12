0498d30f9785e98437588d6390cae2264e620f31
Hungwe <taonashuro@gmail.com>
2023-10-12 05:54:02 -0700

    Variadic functions tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers seperatred by a string
+ * @separator: Initial segment pointer
+ * @n: Number of arguments
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+int num;
+va_list args;
+va_start(args, n);
+if (separator == NULL)
+{
+printf("\n");
+return;
+}
+else
+{
+for (i = 0; i < n - 1; i++)
+{
+num = va_arg(args, int);
+printf("%d", num);
+printf("%s", separator);
+}
+for (i = n - 1; i < n; i++)
+{
+num = va_arg(args, int);
+printf("%d", num);
+}
+printf("\n");
+}
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Print string separated by character
+ * @separator: Pointer for string separator
+ * @n: Pointer for bytes to accept from
+ *
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+const char *str;
+va_list args;
+va_start(args, n);
+if (separator == NULL)
+{
+printf("\n");
+return;
+}
+else
+{
+for (i = 0; i < n - 1; i++)
+{
+str = va_arg(args, const char *);
+if (str == NULL)
+{
+printf("(nil)");
+printf("%s", separator);
+}
+else
+{
+printf("%s", str);
+printf("%s", separator);
+}
+}
+for (i = n - 1; i < n; i++)
+{
+str = va_arg(args, const char *);
+if (str == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", str);
+}
+}
+printf("\n");
+}
+}
